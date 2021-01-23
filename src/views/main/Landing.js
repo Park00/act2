d0200c70de5a7488d22954060d8ddfd4c89df88f
inae <bbaaginae@gmail.com>
2021-01-24 03:06:35 +0900

    랜딩페이지 init

---
+++ src/views/main/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PrimarySearchAppBar from "../main/PrimarySearchAppBar"
+
+export default function Landing(){
+  return(
+    <div>
+      <PrimarySearchAppBar></PrimarySearchAppBar>
+    </div>
+  )
+}
